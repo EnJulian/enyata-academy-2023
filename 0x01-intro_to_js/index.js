ab3d402d36c26635678105dd828186fbbf81d149
Julian <36317786+EnJulian@users.noreply.github.com>
2023-05-25 21:28:50 +0000

    add tasks 0x04, 0x05 and 0x06

---
+++ 0x01-intro_to_js/index.js
@@ -0,0 +1,22 @@
+﻿var myName = "Julian";
+console.log(myName);
+
+typeof myName;
+
+var myName = "Amoah";
+
+const car = {
+    name: "AC 302 (01-07)",
+    power: "225bhp",
+    acceleration: "5.3s",
+    dimensions: "4200mm x 1746mm",
+    topSpeed: "145mph",
+    isNew: "yes",
+    torque: "407 Nm, 300 ft-lb",
+    length: "4200mm",
+    width: "1746mm",
+    height: "1200mm"
+};
+
+console.log(myName); 
+
---
+++ 0x01-intro_to_js/numbers.js
@@ -0,0 +1,13 @@
+﻿const favoriteNumber = 2;
+
+const halfOfFavNum = favoriteNumber / 2;
+
+let isaacsFavorite = 13;
+isaacsFavorite = isaacsFavorite - favoriteNumber ;
+console.log(isaacsFavorite);
+
+isaacsFavorite = isaacsFavorite * 26;
+console.log(isaacsFavorite);
+
+isaacsFavorite = isaacsFavorite % 3;
+console.log(isaacsFavorite);
